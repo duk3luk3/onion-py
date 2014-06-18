1a12794eb96e9d5b5e4cdc0fa27a97f1161e7b70
Lukas Erlacher <l.erlacher@gmail.com>
2014-06-18 12:52:44 +0200

    Version 0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="OnionPy",
-    version="0.1.5",
+    version="0.1.6",
     description="Python wrapper for the OnionOO tor status API",
     long_description=open('README.txt').read(),
     author="Lukas Erlacher",
