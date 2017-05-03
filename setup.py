43c4bd763a237fdd085dd242e334766ff7a84141
Lukas Erlacher <l.erlacher@googlemail.com>
2017-05-03 14:21:42 +0200

    Bump version for PR#8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="OnionPy",
-    version="0.4.0",
+    version="0.5.0",
     description="Python wrapper for the OnionOO tor status API",
     long_description=open('README.txt').read(),
     author="Lukas Erlacher",
